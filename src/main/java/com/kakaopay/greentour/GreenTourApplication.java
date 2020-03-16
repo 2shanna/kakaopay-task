12b6b82fc8414b527112cfae0e24e4613d06b69f
Seunghyun Lee <ilovesh1213@gmail.com>
2020-03-16 23:20:42 +0900

    initial setting

---
+++ src/main/java/com/kakaopay/greentour/GreenTourApplication.java
@@ -0,0 +1,11 @@
+package com.kakaopay.greentour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenTourApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GreenTourApplication.class, args);
+    }
+}
